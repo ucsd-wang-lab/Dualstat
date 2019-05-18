d7601ff766a6e41df1b8f3d2e153d180b22aa260
Chris Brown <cjb008@ucsd.edu>
2019-05-17 18:13:13 -0700

    Added I2C module, WIP writing DAC control functions

---
+++ DualStat/Design01.cydsn/ad5647r.c
@@ -0,0 +1,58 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "main.h"
+#include "i2c.h"
+#include "ad5647r.h"
+
+void setDAC(uint16_t out, DAC_Channel_t ch)
+{
+    /* Command sequence, 3 bytes total
+    //  Write Address with R/W = 0
+    //  Write Command byte
+    //      R -> reserved
+    //      S -> Byte selection: 0=3 bytes, 1=2 bytes 
+    //      C0, C1, C2  -> Commands
+                0=write to input register n
+                1=update dac register n
+                2=write to input register n, update all (software LDAC)
+                3=write to and update DAC channel n
+                4=power up/power down
+                5=reset
+                6=LDAC register setup
+                7=internal reference on/off
+    //      A1, A2, A3  -> Address(n):0=CH_A, 1=DAC_B, 7=BOTH 
+    //  Write MSB of data
+    //  Write LSD of data
+    */
+    
+    uint8_t cmd_byte;//0001 1 A1A2A3
+    switch(ch)
+    {
+        case CH_A:
+            cmd_byte=0x18;
+        break;
+        case CH_B:
+            cmd_byte=0x19;
+        break;
+        case CH_BOTH:
+            cmd_byte=0x1F;
+        break;
+    }
+    
+    uint8_t data[2]; 
+    data[0] = (out >> 8);
+    data[1] = out & 0xFF;   //TODO: check this, MSB needs to be sent first
+       
+    I2CWriteBytes(AD5647R_ADDR, cmd_byte, 2, data);//regAddr is just the first byte written after device addr, used as cmd byte
+}
+
+/* [] END OF FILE */
---
+++ DualStat/Design01.cydsn/ad5647r.h
@@ -0,0 +1,21 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef AD5647R_H
+#define AD5647R_H
+    
+#define AD5647R_ADDR    0x0F    //7 bit, 8 bit 0x1E
+
+typedef enum { CH_A, CH_B, CH_BOTH } DAC_Channel_t;
+    
+    
+#endif /* AD5647R_H */ 
+/* [] END OF FILE */
